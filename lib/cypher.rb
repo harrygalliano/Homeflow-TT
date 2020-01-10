cc3f5ccb5bef6e6ad625cc9958ec4011b78bac9c
Harry Galliano <harrygalliano97@gmail.com>
2020-01-10 09:27:38 +0000

    first test passed

--- lib/cypher.rb
+++ lib/cypher.rb
@@ -1,3 +1,13 @@
 class Cypher 
 
+    def initialize(word, num)
+        @word = word 
+        result = ""
+    end 
+
+    def down_word 
+        d_word = @word.downcase 
+    end 
+
 end 
+
--- spec/cypher_spec.rb
+++ spec/cypher_spec.rb
@@ -1,5 +1,15 @@
 require './lib/cypher'
 
 describe Cypher do 
+    before(:each) do 
+        @cypher = Cypher.new("Hello", 2)
+    end
+
+    describe "it takes parameters when initialized" do 
+        it "takes a string" do 
+            expect(@cypher.down_word).to eq("hello")
+        end 
+    end 
+
 
   end
